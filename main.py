b6ec141b2a582e7ed10e385ae3a8b59369919f08
gokulkrish10 <109692235+gokulkrish10@users.noreply.github.com>
2025-06-12 21:46:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-st.write("🔝Use the **Sidebar** to paste the Links")
+st.write("⤴️Use the **Sidebar** to paste the Links")
 
 # Custom Vector Store Class to replace FAISS
 class SimpleVectorStore:
