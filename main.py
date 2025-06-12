7811a6bc44c6cf018e8f04c7229480fb85ebd85b
gokulkrish10 <109692235+gokulkrish10@users.noreply.github.com>
2025-06-12 19:51:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,14 +51,14 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-hide_streamlit_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# hide_streamlit_style = """
+#     <style>
+#     #MainMenu {visibility: hidden;}
+#     footer {visibility: hidden;}
+#     header {visibility: hidden;}
+#     </style>
+#     """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("""
 <style>
